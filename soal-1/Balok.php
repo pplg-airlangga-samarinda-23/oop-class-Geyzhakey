d06c2711532550454965a61b1f3effdd1f9dcc55
gey <gey@gmail.com>
2024-10-03 14:40:12 +0800

    tugas selesai

--- soal-1/Balok.php
+++ soal-1/Balok.php
@@ -2,16 +2,41 @@
 class Balok
 {
     // deklarasikan atribut-atribut kelas Balok
+    private $lebar;
+    private $panjang;
+    private $tinggi;
 
     
     // definisikan metode konstruktor, setter ,dan getter
+    public function __construct($panjang,$lebar,$tinggi) {
+        $this->panjang = $panjang;
+        $this->lebar = $lebar;
+        $this->tinggi = $tinggi;   
+    }
+    public function __get($atribut) {
+        return $this->$atribut;
+    }
+    public function __set($atribut, $nilai) {
+        $this->$atribut = $nilai;
+    }
 
+    // definisikan metode menghitung volume, luas permukaan,
+    public function hitungvolume() {
+        return $this->panjang * $this->lebar * $this->tinggi;
+    }
+    public function hitungluas() {
+        return 2 * (($this->panjang * $this->lebar) + ($this->panjang * $this->tinggi) + ($this->lebar * $this->tinggi));
+    }
 
-    // definisikan metode menghitung volume, luas permukaan, 
 }
 
 $balok_1 = new Balok(4, 8, 5);
 $balok_2 = new Balok(3, 10, 12);
+echo "jawaban balok 1 adalah" . $balok_1->hitungvolume() . "<br>";
+echo "jawaban balok 1 adalah" . $balok_1->hitungluas() . "<br>";
+echo "<br>";
+echo "jawaban balok 2 adalah" . $balok_2->hitungvolume() . "<br>";
+echo "jawaban balok 2 adalah" . $balok_2->hitungluas();
 
 // contoh output: Balok
 
--- soal-2/Siswa.php
+++ soal-2/Siswa.php
@@ -1,24 +1,54 @@
-<?php
+    <?php
 
-class Siswa
-{
-    // deklarasikan atribut-atribut kelas Siswa
+    class Siswa
+    {
+        // deklarasikan atribut-atribut kelas Siswa
+        private $nama;
+        private $nisn;
+        private $alamat;
+        private $Jurusan;
+        private $tanggallahir;
+        // definisikan metode konstruktor, setter ,dan getter
+        public function __construct($nama,$nisn,$alamat,$Jurusan,$tanggallahir) {
+            $this->nama = $nama;
+            $this->nisn = $nisn;
+            $this->alamat = $alamat;
+            $this->jurusan = $Jurusan;
+            $this->tanggallahir = $tanggallahir;
+        }
+        public function gettanggallahir() {
+        return $this-> $tanggallahir;
+        }    
+        public function __get($atribut) {
+            return $this->$atribut;
+        }
+        public function __set($atribut, $nilai) {
+            $this->$atribut = $nilai;
+        }
 
-    
-    // definisikan metode konstruktor, setter ,dan getter
-
-
-    // buat metode untuk menghitung umur siswa
-    public function hitungUmur() {
-
-
-        return $umur;
+        // buat metode untuk menghitung umur siswa
+        public function hitungUmur() {
+            $tanggallahir = datetime::createfromformat('d-m-Y', $this->tanggallahir);
+            $today = new datetime(); 
+            $umur= $today->diff($tanggallahir);
+            return $umur->y;
+        }
     }
-}
 
-// buat dua buah objek siswa
-$siswa_1 = new Siswa("Bambang", "1234", "Loa Bakung", "PPLG", "27-11-2006");
-$siswa_2 = new Siswa("Bahrun", "1235", "Air Putih", "PPLG", "13-01-2007");
+    // buat dua buah objek siswa
+    $siswa_1 = new Siswa("Bambang", "1234", "Loa Bakung", "PPLG", "27-11-2004");
+    $siswa_2 = new Siswa("Bahrun", "1235", "Air Putih", "PPLG", "13-01-2007");
 
-// contoh output:
-// Bambang lebih tua dibandingkan Bahrun.
+    $umurbambang = $siswa_1->hitungUmur();
+    $umurbahrun = $siswa_2->hitungUmur();
+
+    if($umurbambang > $umurbahrun) {
+        echo "bambang lebih tua dibandingkan bahrun";  
+    } elseif ($umurbahrun > $umurbambang){
+        echo "bahrun lebih tua dibandingkan bambang";
+    } else  {
+        echo "sorry your laptop is error :(";
+    }
+
+    // contoh output:
+    // Bambang lebih tua dibandingkan Bahrun.
